12375309322320379017f82e4742a31e5d7b2d40
Dan Sabadis <dsabadis@sdl.com>
2020-09-14 19:06:19 +0300

    done loops chapter

--- src/Fundamentals/loops.go
+++ src/Fundamentals/loops.go
@@ -6,10 +6,28 @@ import (
 )
 
 func main() {
-	for timer := 10; timer >= 0; timer-- {
+	for timer := 2; timer >= 0; timer-- {
 		if timer == 0 {
 			fmt.Println("BOOM!")
 			break
+		}
+		fmt.Println(timer)
+		time.Sleep(1 * time.Second)
+	}
+
+	coursesInProg := []string{"docker1", "C#2", "Python3"}
+	//coursesCompleted := []string{"javascript1", "SQL2", "Rucy on Rails"}
+	for _, i := range coursesInProg {
+		fmt.Println(i)
+	}
+
+	for j, i := range coursesInProg {
+		fmt.Println(i, j)
+	}
+
+	for timer := 10; timer >= 0; timer-- {
+		if timer%2 == 0 {
+			continue
 		}
 		fmt.Println(timer)
 		time.Sleep(1 * time.Second)
